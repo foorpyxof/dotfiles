5d1df86128d5abd84b57eba2a89b0fc775ad03c3
Erynn Scholtes <erynn@goodgirl.dev>
2025-11-20 19:19:49 +0100

    remove kitty bell noise, remove nvim brainrot

--- nvim/lua/plugins/brainrot.lua
+++ nvim/lua/plugins/brainrot.lua
@@ -3,9 +3,9 @@ return {
   event = "VeryLazy",
   opts = {
     phonk_time = 1.5, -- seconds the phonk/image overlay stays
-    disable_phonk = false, -- skip phonk/overlay on "no errors"
-    sound_enabled = true, -- enable sounds
-    image_enabled = true, -- enable images (needs image.nvim)
+    disable_phonk = true, -- skip phonk/overlay on "no errors"
+    sound_enabled = false, -- enable sounds
+    image_enabled = false, -- enable images (needs image.nvim)
     volume = 50, -- 0..100
     lsp_wide = true,
     min_error_duration = 4,
